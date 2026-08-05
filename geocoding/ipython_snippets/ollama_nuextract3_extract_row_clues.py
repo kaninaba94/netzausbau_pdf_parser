34280341d7f397e78091047619d7034d202d679a
Kaspar Naraghi <kaninaba94@gmail.com>
2026-08-05 10:11:35 +0000

    Enable resuming of row clue extraction

--- geocoding/ipython_snippets/ollama_nuextract3_extract_row_clues.py
+++ geocoding/ipython_snippets/ollama_nuextract3_extract_row_clues.py
@@ -1,11 +1,42 @@
 import json
 from enum import Enum
 from tqdm import tqdm
+from pathlib import Path
 
 from pydantic import BaseModel, ValidationError
 from ollama import chat
+import pandas as pd
 
 from lib.measures import collect_raw_measures_dfs_from_csvs 
+
+OUT_DIR = Path('./artefacts')
+IN_PATH = Path('../pdfplumber_table_extraction/output/00080_Stromnetz Berlin GmbH/Netzausbauplan Stromnetz Berlin GmbH 2024 (Anpassung Maßnahmeplan, Anlage 2).pdf.0.csv')
+
+class SystemPrompt:
+    def __init__(self, instructions: str, rules: str, schema: dict, examples_string: str) -> None:
+        self.instructions = instructions
+        self.rules = rules
+        self.schema = schema
+        self.examples_string = examples_string
+        
+    def __str__(self) -> str:
+        return f"""
+            Instructions:
+            {self.instructions}
+            
+            Rules:
+            {self.rules}
+            
+            JSON schema:
+            {json.dumps(self.schema, ensure_ascii=False)}
+            
+            Examples:
+            {self.examples_string}
+            
+            """
+            
+if not 'all_requests' in globals():
+    all_requests: list = []
 
 class AssetType(str, Enum):
     power_line: str = 'power_line'
@@ -31,50 +62,60 @@ for i in range(len(labels)):
 examples = [(_jsonable(m), l) for m, l in zip(measures, labels, strict=True)]
 examples_string = '\n\n\n'.join([e[0].replace('nan', 'null') + '\n ------------->\n' + str(e[1]) for e in examples])
 
-measures_dfs = collect_raw_measures_dfs_from_csvs('../pdfplumber_table_extraction/')
-df = measures_dfs[1]
+out_path = OUT_DIR / IN_PATH.parent.name / IN_PATH.name
 
-for row_idx in tqdm(range(5)):    
+if out_path.exists():
+    df = pd.read_csv(out_path)
+    df_result = df.copy()
+else:
+    df = pd.read_csv(IN_PATH)
+    df_result = df.copy()
+    df_result['inferred:location_clues'] = None
+    df_result['inferred:asset_type'] = None
+    
+
+row_indices = df.index[(df['inferred:location_clues'].isna() & df['inferred:asset_type'].isna())].tolist()
+
+for row_idx in tqdm(row_indices):#df.shape[0])):    
     row = df.iloc[row_idx].to_dict()
     
-    schema = ExtractionResult.model_json_schema()
+    schema: dict = ExtractionResult.model_json_schema()
     instructions = """
-    The examples show exemplary location_clues under the key 'label:location_clues' and exemplary asset_types under 'label:asset_type'.
-    For location clues, only extract names of streets, municipalities, areas, districts, or regions. No domain-specific abbreviations such as UW, GmbH or HKW.
-    """
-    
-    prompt = f"""
-    {instructions}
     Extract location clues and asset type from the row.
     
-    Rules:
-    - Return ONLY JSON.
-    - No markdown.
-    - No explanation.
-    - location_clues must be a list of strings.
-    - asset_type must be one of: power_line, substation, generator, null.
-    - If unclear, use null.
-    
-    JSON schema:
-    {json.dumps(schema, ensure_ascii=False)}
-    
-    Examples for in-context-learning:
-    {examples_string}
-    
-    Input row:
-    {row}
+    For location clues, only extract names of streets, municipalities, areas, districts, or regions. No domain-specific abbreviations such as UW, GmbH or HKW.
     """
+    rules = """
+    
+            - Return ONLY JSON.
+            - No markdown.
+            - No explanation.
+            - location_clues must be a list of strings.
+            - asset_type must be one of: power_line, substation, generator, null.
+            - If unclear, use null.
+    """ 
+    system_prompt = SystemPrompt(instructions=instructions, schema=schema, rules=rules, examples_string=examples_string) 
     request = {
         'model': "nuextract3-gguf",
-        'messages': [{"role": "user", "content": prompt}], 
+        'messages': [
+            {"role": "system", "content": str(system_prompt)},
+            {"role": "user", "content": str(row)}], 
         'format': schema,
         'options': {
             "temperature": 0,
             "num_ctx": int(2**14),
             "num_predict": 512,
         },
+        "think": False 
     }
     response = chat(
-        **request
+       **request
     )
     all_requests.append((request, response))
+    
+    prediction_dict = json.loads(response.message.content)
+    df_result.loc[row_idx, 'inferred:location_clues'] = str(prediction_dict['location_clues'])
+    df_result.loc[row_idx, 'inferred:asset_type'] = prediction_dict['asset_type']
+    
+    out_path.parent.mkdir(exist_ok=True)
+    df_result.to_csv(out_path, index=False)
